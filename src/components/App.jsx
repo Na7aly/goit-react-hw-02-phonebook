04b666e31660fb5500dae2a36f5517895f83220c
Na7aly <tomac.natalia95@gmail.com>
2024-04-10 13:54:47 +0300

    work on contactform

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
-export const App = () => {
+import React, { useState } from 'react';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import styles from '../components/ContactForm.module.css'
+
+const App = () => {
+  const initialContacts = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+
+  const [contacts, setContacts] = useState(initialContacts);
+  const [filter, setFilter] = useState('');
+
+  const addContact = newContact => {
+    if (contacts.some(contact => contact.name.toLowerCase() === newContact.name.toLowerCase())) {
+      alert('This name is already in contacts.');
+      return;
+    }
+    setContacts(prevContacts => [...prevContacts, newContact]);
+  };
+
+  const deleteContact = id => {
+    setContacts(prevContacts => prevContacts.filter(contact => contact.id !== id));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styles.page}>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} setFilter={setFilter} />
+      <ContactList contacts={contacts} filter={filter} onDeleteContact={deleteContact} />
     </div>
   );
 };
+
+export default App;
