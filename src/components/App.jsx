8ee6ef9c1a850bc01bb0d3c17412fc2b83021bd1
Na7aly <tomac.natalia95@gmail.com>
2024-04-19 00:46:25 +0300

    localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
-import styles from '../components/ContactForm.module.css'
+import styles from '../components/ContactForm.module.css';
+import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
-  const initialContacts = [
+  const initialContacts = JSON.parse(localStorage.getItem('contacts')) || [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
@@ -15,12 +16,17 @@ const App = () => {
   const [contacts, setContacts] = useState(initialContacts);
   const [filter, setFilter] = useState('');
 
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
   const addContact = newContact => {
     if (contacts.some(contact => contact.name.toLowerCase() === newContact.name.toLowerCase())) {
       alert('This name is already in contacts.');
       return;
     }
-    setContacts(prevContacts => [...prevContacts, newContact]);
+    const contactWithId = { ...newContact, id: uuidv4() };
+    setContacts(prevContacts => [...prevContacts, contactWithId]);
   };
 
   const deleteContact = id => {
